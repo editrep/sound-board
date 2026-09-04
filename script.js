63d4f9bde69b3236b1a9231df65db61ff7a2dcdf
editrep <edithedowney@icloud.com>
2026-09-03 20:30:40 -0400

    add function to stop audio overlaping

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
 
 //loops for each sound in the array
 sounds.forEach((sound) => {
+
     //creates a button
     const btn = document.createElement('button');
+
     //add class of 'btn' to button element
     btn.classList.add('btn');
+
     //inner text from sound array
     btn.innerText = sound;
+
+    //add event listener to button, listens for click event
+    btn.addEventListener('click', () => {
+        
+        document.getElementById(sound).play();
+    });
+    
     //add button to the DOM
     document.getElementById('buttons').appendChild(btn);
 });
